d6dd462772f7951a8993b08e310dc98927960ded
Philipp Hamer <ponzellus@gnuzifer.de>
2014-05-14 12:38:53 +0200

    [gitColorLogMessage] changed next-color-calculation

--- gitColorLogMessage.pl
+++ gitColorLogMessage.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 
 my %cols;
-my $maxcol = 30;
+my $maxcol = 0;
 while( <> ) {
 	my @tmp = split /-/, $_, 2;
 	if( $tmp[1] =~ m/^ \[([0-9a-zA-Z:,\._]+)\] (.*)/ ) {
 		my ($topic, $rest) = ($1, $2);
 		if( ! exists $cols{$topic} ) {
-			$maxcol = ( $maxcol + 1 > 36 ) ? 31 : $maxcol+1;
+			$maxcol = 31 + $maxcol++ % 6;
 			$cols{$topic} = $maxcol;
 		}
 		$topic = "\e[1;".$cols{$topic}."m[".$topic."]\e[0m";
