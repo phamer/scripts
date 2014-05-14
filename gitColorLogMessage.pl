c5e72394e87081e1f1b40204dea756da4e12291b
Philipp Hamer <ponzellus@gnuzifer.de>
2014-05-14 12:41:28 +0200

    [gitColorLogMessage] no need to have a space after closing]

--- gitColorLogMessage.pl
+++ gitColorLogMessage.pl
@@ -4,14 +4,14 @@ my %cols;
 my $maxcol = 0;
 while( <> ) {
 	my @tmp = split /-/, $_, 2;
-	if( $tmp[1] =~ m/^ \[([0-9a-zA-Z:,\._]+)\] (.*)/ ) {
+	if( $tmp[1] =~ m/^ \[([0-9a-zA-Z:,\._]+)\](.*)/ ) {
 		my ($topic, $rest) = ($1, $2);
 		if( ! exists $cols{$topic} ) {
 			$maxcol = 31 + $maxcol++ % 6;
 			$cols{$topic} = $maxcol;
 		}
 		$topic = "\e[1;".$cols{$topic}."m[".$topic."]\e[0m";
-		$tmp[1] = " ".$topic." ".$rest."\n";
+		$tmp[1] = " ".$topic.$rest."\n";
 	}
 	print join "-", @tmp;
 
