c8cac63b4e366341a1d4d68cc6630d12e2a25845
Philipp Hamer <philipp@gnuzifer.de>
2015-05-23 18:30:25 +0200

    [gitcolorlogmessage] color is now case-insensitive of topic

--- gitColorLogMessage.pl
+++ gitColorLogMessage.pl
@@ -6,11 +6,12 @@ while( <> ) {
 	my @tmp = split /-/, $_, 2;
 	if( $tmp[1] =~ m/^ \[([0-9a-zA-Z:,\._]+)\](.*)/ ) {
 		my ($topic, $rest) = ($1, $2);
-		if( ! exists $cols{$topic} ) {
+		my $lctopic = lc($topic);
+		if( ! exists $cols{ $lctopic } ) {
 			$maxcol = 31 + $maxcol++ % 6;
-			$cols{$topic} = $maxcol;
+			$cols{ $lctopic } = $maxcol;
 		}
-		$topic = "\e[1;".$cols{$topic}."m[".$topic."]\e[0m";
+		$topic = "\e[1;".$cols{ $lctopic }."m[".$topic."]\e[0m";
 		$tmp[1] = " ".$topic.$rest."\n";
 	}
 	print join "-", @tmp;
